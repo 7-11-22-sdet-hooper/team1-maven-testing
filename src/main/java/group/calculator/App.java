65a9eea9faecfd1ff5f970920adc65cf6a2f13ce
Nathan Shaddock <na.shaddock@gmail.com>
2022-08-04 10:59:17 -0500

    tetst

--- src/main/java/group/calculator/App.java
+++ src/main/java/group/calculator/App.java
@@ -5,6 +5,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello from the main method");
     System.out.println("Test test test");
+    System.out.println("test test");
   }
    
 }
